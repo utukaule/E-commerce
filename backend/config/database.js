da13b162694eb44cbecd6e0940c7e90b961c80e9
Utkatsh Kaule <utkarshkaule7@gmail.com>
2024-04-04 21:05:04 +0530

    done with home page and single product page

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongo://localhost:27017/Econmmerce",{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
-.then((data)=>{
-    console.log(`MongoDB connected to server : ${data.connection.host}`);
-}).catch((err)=>{
-    console.log(err);
-})
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-
-//config
-dotenv.config({path:"backend/config/config.env"})
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server is working on http://localhost:${process.env.PORT}`) 
-})
